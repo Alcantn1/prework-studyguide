75b5bc4c98cdb62a86a3974f1495a3b927febec8
Nico Alcantara <nalcantara92@gmail.com>
2022-11-29 21:14:15 -0800

    debugged javascript

--- assets/assets/script.js
+++ assets/assets/script.js
@@ -5,17 +5,14 @@ function listTopics() {
     console.log(topics[x]);
   }
 }
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
-   }
 function selectTopic() {
-if (randomtopic === 'HTML') {
+if (randomTopic === 'HTML') {
   console.log("Let's study HTML!");
-} else if (randomtopic === 'CSS') {
+} else if (randomTopic === 'CSS') {
   console.log("Let's study CSS!");
-} else if (randomtopic === 'Git') {
+} else if (randomTopic === 'Git') {
   console.log("Let's study Git!");
-} else if (randomtopic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
